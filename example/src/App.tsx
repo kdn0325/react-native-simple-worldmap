ecf2397d9b4746209668249086934e5e2ab8fb0b
kdn0325 <dn10003@gmail.com>
2025-02-22 03:30:29 +0900

    style: add horizontal padding to container in App component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -12,6 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal: 20,
     alignItems: 'center',
     justifyContent: 'center',
   },
