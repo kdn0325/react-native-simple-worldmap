51a0a73f00b61fa7216a24d4d22eadba4d8c22db
kdn0325 <dn10003@gmail.com>
2025-02-22 19:54:43 +0900

    feat: add CountryPath component for interactive country handling

---
+++ src/components/CountryPath.tsx
@@ -0,0 +1,32 @@
+// src/components/CountryPath.tsx
+
+import React, { useRef } from 'react';
+import { PanResponder } from 'react-native';
+import { Path } from 'react-native-svg';
+
+interface CountryPathProps {
+  id: string;
+  d: string;
+  fill?: string;
+  handleCountryClick: (id: string) => void;
+}
+
+const CountryPath: React.FC<CountryPathProps> = ({
+  id,
+  d,
+  fill,
+  handleCountryClick,
+}) => {
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: (e, gestureState) => {
+        handleCountryClick(id);
+      },
+    })
+  ).current;
+
+  return <Path d={d} fill={fill} {...panResponder.panHandlers} />;
+};
+
+export default CountryPath;
